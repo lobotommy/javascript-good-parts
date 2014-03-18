af6485d7b2535cc2c6fa4dcfb256ad536cf08348
Tommy Wendelborg <wendelborg@gmail.com>
2014-03-18 20:03:26 +0100

    test2

--- 0.js
+++ 0.js
@@ -2,6 +2,16 @@ function funky(o) {
   o = null;
 }
 
-var x = [];
-funky(x);
+//var x = [];
+//funky(x);
+//alert(x);
+
+function swap(a, b) {
+  var temp = a;
+  a = b;
+  b = temp;
+}
+
+var x = 1, y = 2;
+swap(x, y);
 alert(x);
