51fd619b014b4d776a7b81a1a691f55dde6bc145
Tommy Wendelborg <wendelborg@gmail.com>
2014-03-18 20:22:48 +0100

    add and mul

--- problem.js
+++ problem.js
@@ -2,5 +2,14 @@ var identity = function(id) {
   return 3;
 }
 
+var add = function(a, b) {
+  return a + b;
+}
 
-alert(identity(3));
+var mul = function(a, b) {
+  return a * b;
+}
+
+//alert(identity(3));
+//alert(add(3, 4));
+//alert(mul(3, 4));
